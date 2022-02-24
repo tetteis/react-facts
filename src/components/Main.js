4dd3c785fcdf21e57e76ccabdf5da0781b5b11d6
Tettei Shahday-Annang <tetteis@gmail.com>
2022-02-24 06:56:54 +0000

    Added basic files

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 40k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../logo.svg" alt="React Logo" className="nav--icon" />
+      <h3 className="nav--logo">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
