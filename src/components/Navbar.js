0c86cd9c9f8f7d1e9ea7e8de624d576c1b6e7c7b
Tettei Shahday-Annang <tetteis@gmail.com>
2022-02-25 09:55:22 +0000

    Modifications and touchup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <img src="../logo.svg" alt="React Logo" className="nav--icon" />
-      <h3 className="nav--logo">ReactFacts</h3>
+      <img src="images/react-logo.png" alt="React Logo" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   );
