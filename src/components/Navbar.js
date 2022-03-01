28a790561fdfd40209392910825429d3a116f463
Tettei Shahday-Annang <tetteis@gmail.com>
2022-03-01 18:06:41 +0000

    Reorganize files and add background image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../images/react-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="images/react-logo.png" alt="React Logo" className="nav--icon" />
+      <img src={logo} alt="React Logo" className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
